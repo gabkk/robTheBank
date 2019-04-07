a2e018d85e44000a2d8b13e1886dae21b7244922
gabkk <gab.kuma@gmail.com>
2019-04-07 10:16:30 +0200

    WIP update contract add new require, getter related to balance, and sending money, made the contract constructor payable

--- flip-coin/contracts/CoinFlip.sol
+++ flip-coin/contracts/CoinFlip.sol
@@ -3,16 +3,37 @@ pragma solidity ^0.5.0;
 contract CoinFlip {
     address owner;
     mapping (address => bool) lastFlip;
+    mapping (address => uint256) userBalance;
 
     event resultOfFlip(string result);
     event calculOfRandom(uint timestamp, uint difficulty, uint result);
 
-    constructor() public {
+    constructor() public payable{
         owner = msg.sender;
+
+        // User address
+	    userBalance[msg.sender] = 0;
+
+	    // Contract address
+	    userBalance[address(this)] = msg.value;
+    	lastFlip[msg.sender] = false;
     }
 
-    function getBalance() view public returns(uint){
+    modifier onlyOwner{
+    	require(owner == msg.sender);
+    	_;
+    }
+
+    function getContract() view public returns(uint){
         return address(this).balance;
+    }
+
+    function getBankBalance() view public returns(uint){
+        return userBalance[address(this)];
+    }
+
+    function getUserBalance(address player) view public returns(uint){
+        return userBalance[player];
     }
 
     function getLastFlip(address player) view public returns(bool){
@@ -25,22 +46,42 @@ contract CoinFlip {
        return result;
    }
 
-    //   NEED TO SECURE THIS
+    //	NEED TO SECURE THIS
+    //	error -1 user balance insuffisant
+    //	error -2 user bet could't be null
+    //	error -3 Contract address don't have enought found to pay the user
+
     function flip() payable public{
-        uint time = random();
+    	require(userBalance[msg.sender] >= msg.value, "-1");
+    	require(msg.value > 0, "-2");
+    	uint256 jackpotValue = msg.value * 2;
+    	require(userBalance[address(this)] >= jackpotValue, "-3");
+        uint randomResult = random();
         uint bet = msg.value;
 
-        if(time % 2 == 0){
-            msg.sender.transfer(bet*2);
+        if(randomResult % 2 == 0){
+        	userBalance[msg.sender] += jackpotValue;
+            userBalance[address(this)] -= jackpotValue;
             lastFlip[msg.sender] = true;
     	    emit resultOfFlip("gagne");
         } else {
+        	userBalance[msg.sender] -= msg.value;
+            userBalance[address(this)] += msg.value;
             lastFlip[msg.sender] = false;
 	        emit resultOfFlip("perd");
         }
     }
 
-    function deposit() payable public{
+    function playerWithdraw(address player) public{
+    	require(msg.sender == player);
+        msg.sender.transfer(userBalance[player]);
+    }
 
+    function sendMoneyToTheBank() payable public{
+    	userBalance[address(this)] += msg.value;
+    }
+
+    function sendMoneyToThePlayer() payable public{
+    	userBalance[msg.sender] += msg.value;
     }
 }
--- flip-coin/migrations/2_deploy_contracts.js
+++ flip-coin/migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var CoinFlip = artifacts.require("./CoinFlip.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(CoinFlip);
+  deployer.deploy(CoinFlip, {value: 100000000});
 };
