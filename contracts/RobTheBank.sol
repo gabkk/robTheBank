cfa7cc9bad24e6953ceb9a9e4c7e3690374f5d80
gabkk <gab.kuma@gmail.com>
2020-02-09 19:07:57 +0100

    add oracle price function, reformat smart contract with the right function order

--- contracts/RobTheBank.sol
+++ contracts/RobTheBank.sol
@@ -27,26 +27,9 @@ contract RobTheBank is usingProvable{
         address payable userAddr;
         address bankAddr;
         uint256 value;
+        uint256 qPrice;
         bool status;
     }
-
-	mapping (address => RobberState) private RobberInfo;
-	mapping (bytes32 => UserInfo) private pendingQueries;
-
-	address public owner;
-
-    uint256 internal constant MAX_INT_FROM_BYTE = 256;
-    uint256 internal constant NUM_RANDOM_BYTES_REQUESTED = 1;
-
-	event ReturnValue(address _sender, uint256 _randomNumber, bool _value);
-	event ReturnValue(bytes32 _queryId, uint256 _randomNumber, bool _value);
-
-    /*
-    * Oracle events
-    */
-    event proofFailed(string description);
-    event LogNewProvableQuery(string description, bytes32 _queryId);
-    event generatedRandomNumber(uint256 randomNumber);
 
     struct Bank {
         string name;
@@ -55,11 +38,28 @@ contract RobTheBank is usingProvable{
         bool usingOracle;
     }
 
-    mapping (address => int256) private userHistory;
+	mapping(address => RobberState) private RobberInfo;
+	mapping(bytes32 => UserInfo) private pendingQueries;
+    mapping(address => int256) private userHistory;
 	mapping(address => Bank) private Banks;
-    address[] public listOfBank;
 
+	event ReturnValue(address _sender, uint256 _randomNumber, bool _value);
+	event ReturnValue(bytes32 _queryId, uint256 _randomNumber, bool _value);
     event LogListOfBank(string name, address addr, uint256 balance);
+    /*
+    * Oracle events
+    */
+    event proofFailed(string description);
+    event LogNewProvableQuery(string description, bytes32 _queryId);
+    event generatedRandomNumber(uint256 randomNumber);
+
+	address public owner;
+
+    uint256 internal constant MAX_INT_FROM_BYTE = 256;
+    uint256 internal constant NUM_RANDOM_BYTES_REQUESTED = 1;
+
+
+    address[] public listOfBank;
 
 	/* Move Modifier to an other file*/
 
@@ -69,6 +69,7 @@ contract RobTheBank is usingProvable{
 
 		setBanks(msg.value, "FED", true, true);
 		setListOfBank(msg.sender);
+		provable_setProof(proofType_Ledger);
 		emit LogListOfBank("FED", msg.sender, msg.value);
 	}
 
@@ -83,29 +84,6 @@ contract RobTheBank is usingProvable{
 		emit LogListOfBank(_name, msg.sender, msg.value);
 	}
 
-    function setBanks(uint256 _balance,
-                      string memory _name,
-                      bool _isCreated,
-                      bool usingOracle)
-                      private {
-		Banks[msg.sender].balance = _balance;
-		Banks[msg.sender].name = _name;
-		Banks[msg.sender].isCreated = _isCreated;
-		Banks[msg.sender].usingOracle = usingOracle;
-    }
-
-    function setListOfBank(address _sender) private{
-        listOfBank.push(_sender);
-    }
-		
-    function getBankLength() view public returns(uint256){
-		return listOfBank.length;
-	}
-
-	function getListOfBank() view public returns(address[] memory){
-		return listOfBank;
-	}
-
 	/* Todo test view */
 	function getListOfBankObj() public {
 		for(uint i=0; i < listOfBank.length; i++){
@@ -117,35 +95,11 @@ contract RobTheBank is usingProvable{
 		}
 	}
 
-	function getBankBalance(address _bankAddr) view public returns(uint256){
-		return Banks[_bankAddr].balance;
-	}
-
-	function getBankName(address _bankAddr) view public returns(string memory){
-		return Banks[_bankAddr].name;
-	}
-
-	function getBankInfos(address _bankAddr) view public returns(string memory, uint256, bool,bool){
-		Bank memory p = Banks[_bankAddr];
-		return (p.name, p.balance, p.isCreated, p.usingOracle);
-	}
-
-	// Get the balance of the user
-	function getUserHistory(address player) view public returns(int256){
-		return userHistory[player];
-	}
-
     function sendMoneyToTheBank() public payable {
         require(msg.value > 0 ether, "msg.value can't 0");
 		require(Banks[msg.sender].isCreated == true, "You don't have a bank yet");
 
 		Banks[msg.sender].balance = Banks[msg.sender].balance.add(msg.value);
-	}
-
-	function isBankOwner(address _myCall) public view returns (bool){
-		if (Banks[_myCall].isCreated == true)
-			return true;
-		return false;
 	}
 
 	function withdrawBankAccount() public payable {
@@ -185,6 +139,67 @@ contract RobTheBank is usingProvable{
         }
     }
 
+	function flip(address _bankAddr, bool _isBankUsingOracle) public payable{
+		// TODO remove this just need to check than we get money to pay the oracle fee
+		require(msg.value < 10 ether, "bet should be less than 10 eth");
+		require(msg.value >= 0.01 ether, "bet should more than 0.01 eth");
+
+		// Check if the player is still waiting for a responce from a previous game
+		if(RobberInfo[msg.sender].userKnownByTheContract == false){
+			RobberInfo[msg.sender].userKnownByTheContract = true;
+			RobberInfo[msg.sender].isAllowToPlay = true;
+		}
+		require(RobberInfo[msg.sender].isAllowToPlay == true,
+			"User is still waiting for the random function to be process");
+		RobberInfo[msg.sender].isAllowToPlay = false;
+
+		// Use a different function regarding if the bank is using an oracle or not
+		if (_isBankUsingOracle == true){
+			_oracleRandom(_bankAddr);
+		} else {
+			uint8 randomResult = _pseudoRandom();
+			flipProcessResult(randomResult, _bankAddr);
+			RobberInfo[msg.sender].isAllowToPlay = true;
+		}
+	}
+
+	function destroyContract() public payable {
+	    require(owner == msg.sender);
+		selfdestruct(msg.sender);
+	}
+
+    function getBankLength()public view returns(uint256){
+		return listOfBank.length;
+	}
+
+	function getListOfBank()public view returns(address[] memory){
+		return listOfBank;
+	}
+
+	function getBankBalance(address _bankAddr)public view returns(uint256){
+		return Banks[_bankAddr].balance;
+	}
+
+	function getBankName(address _bankAddr)public view returns(string memory){
+		return Banks[_bankAddr].name;
+	}
+
+	function getBankInfos(address _bankAddr)public view returns(string memory, uint256, bool,bool){
+		Bank memory p = Banks[_bankAddr];
+		return (p.name, p.balance, p.isCreated, p.usingOracle);
+	}
+
+	// Get the balance of the user
+	function getUserHistory(address player) public view returns(int256){
+		return userHistory[player];
+	}
+
+	function isBankOwner(address _myCall) public view returns (bool){
+		if (Banks[_myCall].isCreated == true)
+			return true;
+		return false;
+	}
+
     function _oracleRandom(address _bankAddr) private{
         uint256 QUERY_EXECUTION_DELAY = 0;
         uint256 GAS_FOR_CALLBACK = 200000;
@@ -193,13 +208,14 @@ contract RobTheBank is usingProvable{
                                 NUM_RANDOM_BYTES_REQUESTED,
                                 GAS_FOR_CALLBACK
                             );
+        pendingQueries[_queryId].qPrice = provable_getPrice("Random");
+        require(msg.value > pendingQueries[_queryId].qPrice, "Bet is smaller than the price for oracle");
         pendingQueries[_queryId].status = true;
         pendingQueries[_queryId].userAddr = msg.sender;
         pendingQueries[_queryId].value = msg.value;
         pendingQueries[_queryId].bankAddr = _bankAddr;
         RobberInfo[msg.sender].queryId = _queryId;
         emit LogNewProvableQuery("Provable query was sent, standing by for the answer...", _queryId);
-        
     }
 
 	/*
@@ -212,24 +228,24 @@ contract RobTheBank is usingProvable{
 		return result;
 	}
 
-    /*
-    *   This function will be called by the standards method
-    */
+	/*
+	*   This function will be called by the standards method
+	*/
     function flipProcessResult(uint8 _randomNumber, address _bankAddr) private returns(bool){
-        bool flipStatus;
-        
-        //TODO ternaire
-        if(_randomNumber % 2 == 0){
+		bool flipStatus;
+
+		//TODO ternaire
+		if(_randomNumber % 2 == 0){
 			Banks[_bankAddr].balance = Banks[_bankAddr].balance.sub(msg.value);
 			msg.sender.transfer(msg.value.mul(2));
 			// Not really safe to cast but the value is high enought to be a problem
 			userHistory[msg.sender] = userHistory[msg.sender].add(int256(msg.value));
 			flipStatus = true;
-        } else {
+		} else {
 			Banks[_bankAddr].balance = Banks[_bankAddr].balance.add(msg.value);
 			// Not really safe to cast but the value is high enought to be a problem
 			userHistory[msg.sender] = userHistory[msg.sender].sub(int256(msg.value));
-		    flipStatus = false;
+			flipStatus = false;
 		}
 		emit ReturnValue(msg.sender, _randomNumber, flipStatus);
         return flipStatus;
@@ -246,6 +262,7 @@ contract RobTheBank is usingProvable{
 
         if(_randomNumber % 2 == 0){
 			Banks[_bankAddr].balance = Banks[_bankAddr].balance.sub(_value);
+			_value = _value.sub(pendingQueries[_queryId].qPrice);
 			_usrAddr.transfer(msg.value.mul(2));
 			// Not really safe to cast but the value is high enought to be a problem
 			userHistory[_usrAddr] = userHistory[_usrAddr].add(int256(_value));
@@ -256,35 +273,24 @@ contract RobTheBank is usingProvable{
 			userHistory[_usrAddr] = userHistory[_usrAddr].sub(int256(_value));
 		    flipStatus = false;
 		}
-        return flipStatus;
+		return flipStatus;
+	}
+
+	function setListOfBank(address _sender) private{
+		listOfBank.push(_sender);
+	}
+
+	function setBanks(
+		uint256 _balance,
+		string memory _name,
+		bool _isCreated,
+		bool usingOracle)
+		private {
+		Banks[msg.sender].balance = _balance;
+		Banks[msg.sender].name = _name;
+		Banks[msg.sender].isCreated = _isCreated;
+		Banks[msg.sender].usingOracle = usingOracle;
     }
 
-	function flip(address _bankAddr, bool _isBankUsingOracle) public payable{
-		// TODO remove this just need to check than we get money to pay the oracle fee
-		require(msg.value < 10 ether, "bet should be less than 10 eth");
-		require(msg.value >= 0.01 ether, "bet should more than 0.01 eth");
-
-        // Check if the player is still waiting for a responce from a previous game
-        if(RobberInfo[msg.sender].userKnownByTheContract == false){
-            RobberInfo[msg.sender].userKnownByTheContract = true;
-            RobberInfo[msg.sender].isAllowToPlay = true;
-        }
-        require(RobberInfo[msg.sender].isAllowToPlay == true,
-                "User is still waiting for the random function to be process");
-        RobberInfo[msg.sender].isAllowToPlay = false;
-
-        // Use a different function regarding if the bank is using an oracle or not
-        if (_isBankUsingOracle == true){
-    	    _oracleRandom(_bankAddr);
-        } else {
-		    uint8 randomResult = _pseudoRandom();
-            flipProcessResult(randomResult, _bankAddr);
-    		RobberInfo[msg.sender].isAllowToPlay = true;
-        }
-	}
-
-	function destroyContract() public payable {
-	    require(owner == msg.sender);
-		selfdestruct(msg.sender);
-	}
 }
+
