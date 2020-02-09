51833e5ad58854a32c81e13afad83b7a4541859b
gabkk <gab.kuma@gmail.com>
2020-02-09 19:30:05 +0100

    increase gas value for deployement

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var RobTheBank = artifacts.require("./RobTheBank.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(RobTheBank, {value: web3.utils.toWei("1", "ether")});
+  deployer.deploy(RobTheBank, {value: web3.utils.toWei("1", "ether"), gas: 4000000});
 };
