185ca4b2407384fe4560046ee5bba7089dd261a6
gabkk <gab.kuma@gmail.com>
2020-02-09 19:38:17 +0100

    change deploy contract value

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var RobTheBank = artifacts.require("./RobTheBank.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(RobTheBank, {value: web3.utils.toWei("1", "ether"), gas: 4000000});
+  deployer.deploy(RobTheBank, {value: web3.utils.toWei("1", "ether"), gas: 6000000});
 };
